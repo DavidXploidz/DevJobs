cb8d0f8bfa13b6fff51a5b7b90da6f1aa4d57490
DavidXploidz <david.adame.itd@gmail.com>
2023-04-04 23:29:31 -0600

    enviar cv de candidato

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -2,10 +2,45 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Vacante;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class PostularVacante extends Component
 {
+    use WithFileUploads;
+    public $cv;
+    public $vacante;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function mount(Vacante $vacante)
+    {
+        $this->vacante = $vacante;
+    }
+    
+    public function postularme()
+    {
+        $datos = $this->validate();
+
+        //Almacenar el CV
+        $cv = $this->cv->store('public/cv');
+        $datos['cv'] = str_replace('public/cv/','',$cv);
+
+        //Crear el candidatos a la vacante
+        $this->vacante->candidatos()->create([
+            'user_id' =>auth()->user()->id,
+            'cv' => $datos['cv']
+        ]);
+
+        //Mostrar mensaje de ok al usuario
+        session()->flash('mensaje','Se envio correctamente tu informacion, mucha suerte');
+
+        return redirect()->back();
+    }
+
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,8 +1,23 @@
 <div class="bg-gray-700 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4 text-gray-100">Postularme a esta vacante</h3>
-    <form class="w-96 mt-5">
-        <div class="mb-4">
-        
-        </div>
-    </form>
+
+    @if (session()->has('mensaje'))
+        <p class="uppercase rounded-lg border border-green-600 bg-green-100 text-green-600 font-bold p-2 my-4">
+            {{session('mensaje')}}
+        </p>
+    @else
+        <form class="w-96 mt-5" wire:submit.prevent='postularme' >
+            <div class="mb-4">
+                <x-input-label for="cv" :value="__('cv')"/>
+                <x-text-input id="cv" class="block mt-1 w-full" type="file" accept=".pdf" wire:model="cv"/>
+            </div>
+            @error('cv')
+                <livewire:mostrar-alerta :message="$message">
+            @enderror
+            <x-primary-button class="my-5">
+                {{__('Postularme')}}
+            </x-primary-button>
+        </form>
+    @endif
+    
 </div>
